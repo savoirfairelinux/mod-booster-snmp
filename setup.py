4927f7eacc0ae1b018f4b03dccf11828b7d85565
Thibault Cohen <thibault.cohen@savoirfairelinux.com>
2014-03-11 13:52:21 -0400

    Add debian folder

---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+setup(
+    name='mod-snmpbooster',
+    version="1.0",
+    description='SNMP booster module for Shinken',
+    author='Thibault Cohen',
+    author_email='thibault.cohen@savoirfairelinux.com',
+    url='https://github.com/titilambert/mod-booster-snmp',
+    license='GPLv3',
+    install_requires=["python-memcached",
+                      "pysnmp",
+                      "pyasn1",
+                      "configobj",
+                      ],
+    packages=find_packages(),
+    package_dir={'snmp_booster': 'shinken/modules/snmp_booster'},
+    include_package_data=True,
+#    namespace_packages=['shinken.modules.snmp_booster'],
+    test_suite='nose.collector',
+    entry_points=""" """,
+)
+
