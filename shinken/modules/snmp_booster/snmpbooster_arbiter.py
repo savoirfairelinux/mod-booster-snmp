9829ea8829e0f578108d76fac40d2286147afe8c
Thibault Cohen <thibault.cohen@savoirfairelinux.com>
2014-10-01 11:48:12 -0400

    Clean datasource file reading

--- shinken/modules/snmp_booster/snmpbooster_arbiter.py
+++ shinken/modules/snmp_booster/snmpbooster_arbiter.py
@@ -16,6 +16,64 @@ class SnmpBoosterArbiter(SnmpBooster):
         SnmpBooster.__init__(self, mod_conf)
         self.nb_tick = 0
 
+        # Read datasource files
+        # Config validation
+        f = None
+        if not isinstance(self.datasource, dict):
+            try:
+                # Test if self.datasource_file, is file or directory
+                #if file
+                if os.path.isfile(self.datasource_file):
+                    self.datasource = ConfigObj(self.datasource_file,
+                                                interpolation='template')
+                    logger.info("[SnmpBooster] [code 59] Reading input configuration "
+                               "file: %s" % self.datasource_file)
+
+                # if directory
+                elif os.path.isdir(self.datasource_file):
+                    if not self.datasource_file.endswith("/"):
+                        self.datasource_file.join(self.datasource_file, "/")
+                    files = glob.glob(os.path.join(self.datasource_file,
+                                                   'Default*.ini')
+                                      )
+                    for f in files:
+                        if self.datasource is None:
+                            self.datasource = ConfigObj(f,
+                                                        interpolation='template')
+                        else:
+                            ctemp = ConfigObj(f, interpolation='template')
+                            self.datasource.merge(ctemp)
+                            logger.info("[SnmpBooster] [code 60] Reading input "
+                                        "configuration file: %s" % f)
+                else:
+                    # Normal error with scheduler and poller module
+                    # The configuration will be read in the database
+                    raise IOError("[SnmpBooster] File or folder not "
+                                  "found: %s" % self.datasource_file)
+
+            # raise if reading error
+            except Exception as e:
+                if f is not None:
+                    error_message = ("[SnmpBooster] [code 61] Datasource error "
+                                     "while reading or merging in %s : "
+                                     "`%s'" % (str(f), str(e)))
+                else:
+                    error_message = ("[SnmpBooster] [code 62] Datasource error "
+                                     "while reading or merging : `%s'" % str(e))
+                logger.error(error_message)
+                raise Exception(error_message)
+
+        # Convert datasource to dict
+        if isinstance(self.datasource, ConfigObj):
+            try:
+                self.datasource = self.datasource.dict()
+            except Exception as e:
+                error_message = ("[SnmpBooster] [code 65] Error during the "
+                                 "config conversion: %s" % (str(e)))
+                logger.error(error_message)
+                raise Exception(error_message)
+
+
     def hook_late_configuration(self, arb):
         """ Read config and fill memcached """
         mac_resol = MacroResolver()
